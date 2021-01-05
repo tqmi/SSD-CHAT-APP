15e839c794a7ba701273c6bc82dd094e5463b660
dragoseflc <54283756+dragoseflc@users.noreply.github.com>
2021-01-05 13:39:32 +0200

    SCA-19 Tied up UI with database
    Assigned predefined Topic choices to entries from database

--- sca/src/TopicsList.js
+++ sca/src/TopicsList.js
@@ -2,47 +2,23 @@ import './TopicsList.css'
 
 import React from 'react';
 
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+import {firebase,auth,firestore} from './firebase'
 
+function TopicsList(props) {
 
+    const topicsRef = firestore.collection('topics');
+    const query = topicsRef.orderBy('name');
+    const [topics] = useCollectionData(query,{idField:'id'});
 
-
-function TopicsList() {
+    function switchTopic(topicID){
+        props.switchTopic(topicID);
+    }
 
     return (<>
+        
         <main>
-        <ul>
-        <li><a href="#Science">Science</a></li>
-        <li><a href="#Politics">Politics</a></li>
-        <li><a href="#Technology">Technology</a></li>
-        <li><a href="#Arts">Arts</a></li>
-        <li><a href="#History">History</a></li>
-        <li><a href="#Architecture">Architecture</a></li>
-        <li><a href="#Philosophy">Philosophy</a></li>
-        <li><a href="#Sports">Sports</a></li>
-        <li><a href="#Law">Law</a></li>
-        <li><a href="#Religion">Religion</a></li>
-        <li><a href="#Biology">Biology</a></li>
-        <li><a href="#Literature">Literature</a></li>
-        <li><a href="#Linguistics">Linguistics</a></li>
-        <li><a href="#Music">Music</a></li>
-        <li><a href="#Food">Food</a></li>
-        </ul>
-            {/*<button>Science</button>
-            <button>Politics</button>
-            <button>Technology</button>
-            <button>Arts</button>
-            <button>History</button>
-            <button>Architecture</button>
-            <button>Philosophy</button>
-            <button>Sports</button>
-            <button>Law</button>
-            <button>Religion</button>
-            <button>Biology</button>
-            <button>Literature</button>
-            <button>Linguistics</button>
-            <button>Music</button>
-            <button>Movies</button>
-            <button>Food</button>*/}
+            {topics && topics.map(topic => <button key={topic.id} onClick={()=>switchTopic(topic.id)}>{topic.name}</button>)}
         </main>
     </>)
 }
