6f0146a05c44ae44232053220cc813839527fe28
Petronela Vieru <petronela.vieru@student.upt.ro>
2021-01-06 21:19:02 +0200

    SCA-17 Changed implementation to a pattern
    It makes it easier to add new features such as buttons

--- sca/src/Menu.js
+++ sca/src/Menu.js
@@ -9,41 +9,9 @@ import { firebase, auth, firestore } from './firebase'
 function Menu() {
 
     return (<>
-
-        <div class="container">
-            <ul class="icon-menu">
-            
-                <li class="icon-box home">
-                <i class="fas fa-home"></i> <a href="#">
-                        <h2>Home</h2></a>
-                </li>
-
-
-                <li class="icon-box design">
-                    <i class="fa fa-paint-brush fa-4x"></i> <a href="#">
-                        <h2>Design</h2></a>
-                </li>
-
-
-                <li class="icon-box coding">
-                    <i class="fa fa-code fa-4x"></i> <a href="#">
-                        <h2>Coding</h2></a>
-                </li>
-
-
-                <li class="icon-box shop">
-                    <i class="fa fa-shopping-cart fa-4x"></i> <a href="#">
-                        <h2>Shop</h2></a>
-                </li>
-
-                
-
-
-            </ul>
-
-
-            
-        </div>
+        <a href="#" class="myButton">HOME</a><br /><br />
+        <a href="#" class="myButton">Button 1</a><br/><br />
+        <a href="#" class="myButton">Button 2</a><br/><br />
     </>);
 
 }
