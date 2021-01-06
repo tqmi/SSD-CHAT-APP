d96ab1e0d4e08f45891f62b0e9b4fa53824664be
Drago <54283756+dragoseflc@users.noreply.github.com>
2021-01-06 22:25:30 +0200

    SCA-8 Developed full TopicsList functionality
    Implemented Search option, set subscribe/unsubscribe feature

--- sca/src/TopicsList.js
+++ sca/src/TopicsList.js
@@ -1,25 +1,40 @@
 import './TopicsList.css'
 
-import React from 'react';
+import React, { useState } from 'react';
 
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 import {firebase,auth,firestore} from './firebase'
 
 function TopicsList(props) {
 
+    
     const topicsRef = firestore.collection('topics');
-    const query = topicsRef.orderBy('name');
+    const [query,setQuery] = useState(topicsRef.orderBy('name'));
     const [topics] = useCollectionData(query,{idField:'id'});
+    const [searchValue,setSearchValue] = useState('');
 
+    const searchTopic = async(e) => {
+        e.preventDefault();
+        setQuery(topicsRef.where('name','==',searchValue));
+        setSearchValue('');
+    }
     
     return (<>
         
         <div ClassName="topics">
-            {topics && topics.map(topic => (
+
+        <form onSubmit={searchTopic}>
+
+            <input value={searchValue} onChange={(e) => setSearchValue(e.target.value)} placeholder="Search" />
+
+            <button type="submit" disabled={!searchValue}>SEARCH</button>
+
+        </form>
+            {topics && topics.map(topic =>{ return (
                 <>
                     <Topic key={topic.id} switchTopic={props.switchTopic} Topic={topic}/>
                 </>
-            ))}
+            )})}
         </div>
     </>)
 
@@ -28,13 +43,34 @@ function TopicsList(props) {
 
 
 function Topic(props) {
-
+    
+    const {uid} = auth.currentUser;
     function switchTopic(topicID){
         props.switchTopic(topicID);
     }
-    return (
+
+    const subscribeToTopic = async(topicID) => {
+        const topicRef = firestore.collection('topics').doc(topicID);
+        await topicRef.update({subscribers: firebase.firestore.FieldValue.arrayUnion(uid)});
+    }
+
+    const unsubscribeToTopic = async(topicID) => {
+        const topicRef = firestore.collection('topics').doc(topicID);
+        await topicRef.update({subscribers: firebase.firestore.FieldValue.arrayRemove(uid)});
+    }
+
+    return (<>
         <button onClick={()=>switchTopic(props.Topic.id)}>{props.Topic.name}</button>
-    );
+
+        {(props.Topic.subscribers && props.Topic.subscribers.includes(uid)) ? 
+                        
+            (<button onClick={()=>unsubscribeToTopic(props.Topic.id)}>unsubscribe</button>)
+            
+            : 
+            
+            (<button onClick={()=>subscribeToTopic(props.Topic.id)}>subscribe</button>)
+        }
+    </>)
 }
 
 export default TopicsList;
